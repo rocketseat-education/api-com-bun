75ab45abb15694474be6cea2d6ba571cec7a8f03
Elias Gabriel <eliasgabrielcf@gmail.com>
2024-01-18 17:54:55 -0300

    Rota de cadastro de restaurante

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,28 @@
 import { Elysia } from 'elysia'
+import { db } from '../db/connection'
+import { restaurants, users } from '../db/schema'
 
-const app = new Elysia().get('/', () => {
-  return 'Hello World!'
+const app = new Elysia().post('/restaurantes', async ({ body, set }) => {
+  const { restaurantName, name, email, phone } = body as any
+
+  const [manager] = await db
+    .insert(users)
+    .values({
+      name,
+      email,
+      phone,
+      role: 'manager',
+    })
+    .returning({
+      id: users.id,
+    })
+
+  await db.insert(restaurants).values({
+    name: restaurantName,
+    managerId: manager.id,
+  })
+
+  set.status = 204
 })
 
 app.listen(3333, () => {
