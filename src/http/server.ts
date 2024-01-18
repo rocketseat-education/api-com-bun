f42602734d97aeac76e66cd47f69aaa44b1dd392
Elias Gabriel <eliasgabrielcf@gmail.com>
2024-01-18 17:56:45 -0300

    Tipagem na entrada de dados

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,29 +1,40 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 import { db } from '../db/connection'
 import { restaurants, users } from '../db/schema'
 
-const app = new Elysia().post('/restaurantes', async ({ body, set }) => {
-  const { restaurantName, name, email, phone } = body as any
+const app = new Elysia().post(
+  '/restaurantes',
+  async ({ body, set }) => {
+    const { restaurantName, managerName, email, phone } = body
 
-  const [manager] = await db
-    .insert(users)
-    .values({
-      name,
-      email,
-      phone,
-      role: 'manager',
+    const [manager] = await db
+      .insert(users)
+      .values({
+        name: managerName,
+        email,
+        phone,
+        role: 'manager',
+      })
+      .returning({
+        id: users.id,
+      })
+
+    await db.insert(restaurants).values({
+      name: restaurantName,
+      managerId: manager.id,
     })
-    .returning({
-      id: users.id,
-    })
 
-  await db.insert(restaurants).values({
-    name: restaurantName,
-    managerId: manager.id,
-  })
-
-  set.status = 204
-})
+    set.status = 204
+  },
+  {
+    body: t.Object({
+      restaurantName: t.String(),
+      managerName: t.String(),
+      phone: t.String(),
+      email: t.String({ format: 'email' }),
+    }),
+  },
+)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
