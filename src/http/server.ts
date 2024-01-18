6ee70e2fce5b722dec4f7d3acbee4e7b22d80fd7
Elias Gabriel <eliasgabrielcf@gmail.com>
2024-01-18 16:54:28 -0300

    Criando API com Bun e Elysia

---
+++ src/http/server.ts
@@ -0,0 +1,10 @@
+import { Elysia } from "elysia";
+
+const app = new Elysia()
+  .get('/', () => {
+    return 'Hello World!'
+  })
+
+app.listen(3333, () => {
+  console.log("🔥 HTTP server running!")
+})
