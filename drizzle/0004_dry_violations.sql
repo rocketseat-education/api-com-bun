b70974b77e3c24f7bfd422a33a805b901c9b2452
Elias Gabriel <eliasgabrielcf@gmail.com>
2024-02-15 13:58:43 -0300

    Schema de links de autenticação

---
+++ drizzle/0004_dry_violations.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS "auth_links" (
+	"id" text PRIMARY KEY NOT NULL,
+	"code" text NOT NULL,
+	"user_id" text NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	CONSTRAINT "auth_links_code_unique" UNIQUE("code")
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "auth_links" ADD CONSTRAINT "auth_links_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,2 +1,3 @@
 export * from './users'
 export * from './restaurantes'
+export * from './auth-links'
